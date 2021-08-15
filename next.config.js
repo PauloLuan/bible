ca47acfa2d76ecba204beb61c171bca8e58cd3af
Paulo Luan <pauloluan.inova@gmail.com>
2021-08-15 04:52:55 -0300

    added pwa config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -11,4 +19,4 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true
   }
-}
+})
