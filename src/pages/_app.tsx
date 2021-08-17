575c97d38f0288636674c45c8ca7e77b5c29dbff
Paulo Luan <pauloluan.inova@gmail.com>
2021-08-17 05:29:52 -0300

    added icons and manifest loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,9 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Bíblia Sagrada</title>
         <meta name="description" content="Bíblia Sagrada Completa" />
+        <meta name="theme-color" content="#1A202C" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/img/maskable_icon_x192.png" />
       </Head>
 
       <Component {...pageProps} />
