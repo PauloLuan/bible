871dc8589c001a833b89fdcf74d90866986ab0fe
Paulo Luan <pauloluan.inova@gmail.com>
2021-08-17 05:41:26 -0300

    added more icons and metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,10 +9,77 @@ function App({ Component, pageProps }: AppProps) {
     <ChakraProvider resetCSS theme={theme}>
       <Head>
         <title>Bíblia Sagrada</title>
-        <meta name="description" content="Bíblia Sagrada Completa" />
+        <meta
+          name="description"
+          content="A palavra de Deus, na palma da sua mão."
+        />
         <meta name="theme-color" content="#1A202C" />
+
+        <meta name="application-name" content="Biblia Sagrada" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Biblia Sagrada" />
+        <meta
+          name="description"
+          content="Biblia Sagrada completa e gratuita!"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/img/icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="256x256"
+          href="/img/icon-256x256.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="384x384"
+          href="/img/icon-384x384.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/img/icon-512x512.png"
+        />
+
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/img/maskable_icon_x192.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://biblia-omega.vercel.app/" />
+        <meta name="twitter:title" content="Biblia Sagrada" />
+        <meta
+          name="twitter:description"
+          content="A palavra de Deus, na palma da sua mão."
+        />
+        <meta
+          name="twitter:image"
+          content="https://biblia-omega.vercel.app//img/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@pauloluan" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Bíblia Sagrada" />
+        <meta
+          property="og:description"
+          content="A palavra de Deus, na palma da sua mão."
+        />
+        <meta property="og:site_name" content="Bíblia Sagrada" />
+        <meta property="og:url" content="https://biblia-omega.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://biblia-omega.vercel.app/img/apple-touch-icon.png"
+        />
       </Head>
 
       <Component {...pageProps} />
